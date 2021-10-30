dbabd9d7d612b23fa13b2324d37cf8b5d3901514
manhlab <manhtranduc1111@gmail.com>
2021-10-31 01:47:19 +0300

    update khanhvy

--- index.js
+++ index.js
@@ -8,30 +8,33 @@ const client = new Client({
 
 let interval;
 client.on("guildMemberAdd", (member) => {
-  member.guild.channels.get("welcome").send("Mình là Khánh Vy, đang học dự bị tại Bách Khoa. Mình phụ trách quản lý page. Rất vui được làm quen với bạn!");
+  member.guild.channels
+    .get("welcome")
+    .send(
+      "Mình là Khánh Vy, đang học dự bị tại Bách Khoa. Mình phụ trách quản lý page. Rất vui được làm quen với bạn!"
+    );
 });
 client.on("message", async (msg) => {
-  var welcome_dict = {
-    hi: "hiii :>",
-    hello: "Chaoooo ban :>",
-    chào: "Hellllo nha :))",
-  };
+  var welcome_dict = ["hi", "hello", "chào"];
+  var hello = ["Добрый день :>", "Chaoooo chụy :>", "Hellllo nha :))", "Привет"];
   if (msg.channel.name === "welcome") {
-    Object.entries(welcome_dict).forEach(([key, value]) => {
-      if (msg.content.toLowerCase().includes(key)) {
-        msg.reply(value);
+    for (var i = 0; i < welcome_dict.length; i++) {
+      if (msg.content.toLowerCase().includes(welcome_dict[i])) {
+        msg.reply(hello[Math.floor(Math.random() * hello.length)]);
+        break;
       }
-    });
+    }
+  } else if (msg.author.tag == "Нгуен#2735") {
+    msg.reply("Aizo mèn ơi:))");
   } else if (msg.content.toLowerCase().includes("hỏi")) {
     msg.reply("Bạn vui lòng chờ phản hồi từ admin nhé!");
-  } 
+  }
   if (msg.content.toLowerCase().includes("vy")) {
     msg.reply("Mình khong có ngu ngok đâu nha :))");
   }
-  
 });
-client.on('ready', () => {
-	console.log(`Logged in as ${client.user.username}!`);
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.username}!`);
 });
 //must be last line
 client.login(process.env.BOT_TOKEN);
